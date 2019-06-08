14b71e83a48652aeca6420c28a3949cda046e87c
Sathish <Sathish@LAPTOP-DO9LOK0B>
2019-06-08 16:39:38 +0530

    First change

---
+++ src/main/java/com/app/SpringBootAppRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppRunnerApplication.class, args);
+	}
+
+}
